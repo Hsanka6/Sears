63015207b211ab4519c05f6b27b0dd2553b9abab
Hsanka6 <haasith2@gmail.com>
2018-06-19 11:34:00 -0500

    Initial commit

---
+++ app/src/main/java/com/sears/android/dlm/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sears.android.dlm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
